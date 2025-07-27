a28ace70e505592dae9b4911437242ab4f7ddd9d
Baiyari <baiyaridebbarma0017@gmail.com>
2025-07-27 12:15:39 +0530

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,23 @@
+import np
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+## Title of the application
+st.title("Hello Everyone This is Our First Application")
+st.write("This is SCIT in Pune!!!")
+
+##create a simple Dataframe
+
+df = pd.DataFrame({
+    'first column': [1,2,3,4],
+    'second column': [10,20,30,40]
+})
+##Display the Dataframe
+st.write("Here is the dataframe")
+st.write(df)
+
+chart_data=pd.DataFrame(
+    np.random.randn(20,3),columns=['a','b,'c']
+)
+st.line_chart(chart_data)
